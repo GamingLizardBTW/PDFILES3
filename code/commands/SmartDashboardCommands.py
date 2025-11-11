20351467f00bac166c31f95eac5b113ee374e515
Luis <luisazamudio1489@gmail.com>
2025-11-10 17:08:45 -0800

    second iteration of smart dashboard command

--- code/commands/SmartDashboardCommands.py
+++ code/commands/SmartDashboardCommands.py
@@ -1,5 +1,5 @@
 import logging
-logger = logging.getLogger("firstmotorsubsystemlogger")
+logger = logging.getLogger("smartdashboardsubsystemlogger")
 
 import wpilib
 import commands2
@@ -15,18 +15,15 @@ class  IncrementNumber(commands2.Command):
 
     def __init__(self, smartdashboardsubsystem: SmartDashboardlSubsystemClass) -> None:
 
-        self.smartdashboardsub.increment_number()
-        current_value = self.smartdashboardsub.get_number()
-        wpilib.SmartDashboard.putNumber("My Stored Number", current_value)
-
         self.smartdashboardsub = smartdashboardsubsystem
         self.addRequirements(self.smartdashboardsub)
 
 
-    #def execute(self):
-        
-        #self.motorsub.go_forward
-        #logger.info("Forward Command Running")
+    def initialize(self):
+        self.smartdashboardsub.increment_number()
+        current_value = self.smartdashboardsub.get_number()
+        wpilib.SmartDashboard.putNumber("My Stored Number", current_value)
+
 
     def isFinished(self):
 
